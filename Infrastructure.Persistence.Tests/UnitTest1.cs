1fdbf255825ce568f0c493c4b312536b72d1e2fe
Rafal <rafals_92@gmail.com>
2020-08-19 05:20:47 -0700

    Introduced Infrastructure.Persistence.Tests project

---
+++ Infrastructure.Persistence.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Infrastructure.Persistence.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
