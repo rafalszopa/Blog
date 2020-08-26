cdb79f70fb6122b87d34a6089d87c5167c53f9d4
Rafal <rafals_92@gmail.com>
2020-08-26 03:09:46 -0700

    Improved Security unit tests

--- Infrastructure.Security.Tests/Authentication/PasswordHasherTests.cs
+++ Infrastructure.Security.Tests/Authentication/PasswordHasherTests.cs
@@ -1,32 +1,76 @@
 ﻿using Infrastructure.Authentication;
+using Infrastructure.Security.Authentication;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Infrastucture.Security.Tests.Authentication
 {
     [TestFixture]
     public class PasswordHasherTests
     {
-        [Test]
-        public void VerifyPassword_ShouldReturnSuccess()
+        [TestCase(null, null)]
+        [TestCase("", new byte[0])]
+        public void VerifyPassword_Should_ReturnFail_When_PassedNullOrEmptyParams(string password, byte[] hash)
         {
+            // Arrange ans Act
             var sut = new PasswordHasher();
-            var result = sut.HashPassword("pa$$wOrd_123");
-            var result2 = sut.HashPassword("pa$$wOrd123");
+            var result = sut.VerifyPassword(password, hash);
 
-            Assert.True(true);
+            // Assert
+            Assert.That(result, Is.EqualTo(PasswordVerificationResult.Failed));
         }
 
         [Test]
-        public void VerifyPassword_ShouldReturnFail()
+        public void VerifyPassword_Should_ReturnSuccess_When_PasswordIsCorrect()
         {
-
+            // Arrnge
+            const string plainPassword = "Admin_123";
             var sut = new PasswordHasher();
+            byte[] hashedPassword = sut.HashPassword(plainPassword);
 
-            //sut.VerifyPassword()
+            // Act
+            var result = sut.VerifyPassword(plainPassword, hashedPassword);
 
+            // Assert
+            Assert.That(result, Is.EqualTo(PasswordVerificationResult.Success));
+        }
+
+        [Test]
+        public void VerifyPassword_Should_ReturnFail_When_PasswordIsWrong()
+        {
+            // Arrnge
+            const string correctPlainPassword = "Admin_123";
+            const string wrongPlainPassword = "Admin123";
+            var sut = new PasswordHasher();
+            byte[] hashedPassword = sut.HashPassword(correctPlainPassword);
+
+            // Act
+            var result = sut.VerifyPassword(wrongPlainPassword, hashedPassword);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(PasswordVerificationResult.Failed));
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void HashPassword_Should_ReturnEmptyByteArray_When_PassedNullOrEmptyPassword(string password)
+        {
+            // Arrange ans Act
+            var sut = new PasswordHasher();
+            var result = sut.HashPassword(password);
+
+            // Assert
+            Assert.That(result, Is.EquivalentTo(new byte[0]));
+        }
+
+        [Test]
+        public void HashPassword_Should_ReturnByteArray()
+        {
+            // Arrange ans Act
+            var sut = new PasswordHasher();
+            var result = sut.HashPassword("Admin_123");
+
+            // Assert
+            Assert.That(result, Is.Not.Empty);
         }
     }
 }
--- Infrastructure.Security.Tests/Models/PasswordHashTests.cs
+++ Infrastructure.Security.Tests/Models/PasswordHashTests.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.Security.Models;
+﻿using Infrastructure.Authentication;
+using Infrastructure.Security.Models;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using NUnit.Framework;
 using System;
@@ -7,126 +8,107 @@ namespace Infrastucture.Security.Tests.Models
 {
     public class PasswordHashTests
     {
-        public string Base64EncodedPasswordHash =>
-            "AQAAABAnAACAAAAAAh1aM2YgEvdSqNfWG10GzqMFncTjBmXaDuvSpAxbtnjNr8ar9aFJ8MhxxKdHUzEIZMmX2UoxEPu3fyV6Tm3q9/FeCWyWW9gBvKz7ckK+k7u5yUBDxN0tnJ/K83V8zqFah8216UnFcQjwlR2eNZ65+expwYUtpjMUMG3NzGdjhe004ELmbPL9OzCq89DGD9JX9JBBqXZQFbIIk8xr1I3zkoIu7Sg00A7fwc67nf9BfJNCIs/1ehY+48qP3dkmkmejtmFNlyGqIiSBYuenlXezxhc5KbG9/Rds7wldpPI1uS9nF+ad9ORxlwCqdBgvl05T3BfC7Gi15blK4acYTH0ySw==";
-
-        public abstract class PasswordHashBase
-        {
-            public abstract void Bytes_Should_ReturnProperByteArray();
-        }
-
-        [TestFixture]
-        public class Ctor_Takes_ByteArray : PasswordHashBase
-        {
-            [Test]
-            public override void Bytes_Should_ReturnProperByteArray()
-            {
-                var bytes = new byte[100];
-
-                var passwordHash = new PasswordHash(bytes);
-
-                Assert.AreEqual(Convert.ToBase64String(bytes), Convert.ToBase64String(passwordHash.Bytes));
-            }
-        }
-
-        [TestFixture]
-        public class Ctor_Takes_HashParameters : PasswordHashBase
-        {
-            [Test]
-            public override void Bytes_Should_ReturnProperByteArray()
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        [Test]
-        public void EqualityOperator_ShouldReturnTrue_WhenHashesAreSame()
-        {
-            bool result = true;
-
-            Assert.IsTrue(result);
-        }
-
-        [Test]
-        public void EqualityOperator_ShouldReturnFalse_WhenHashesAreNotSame()
-        {
-            bool result = false;
-
-            Assert.IsFalse(result);
-        }
-
-        [Test]
-        public void Should_CreateCorrect()
+        [TestCase(KeyDerivationPrf.HMACSHA1, 100, 64)]
+        [TestCase(KeyDerivationPrf.HMACSHA256, 1_000, 256)]
+        [TestCase(KeyDerivationPrf.HMACSHA512, 1_000, 512)]
+        public void Ctors_ShouldCreateEqualObjects(KeyDerivationPrf keyDerivationPrf, int iterationCount, int saltLength)
         {
             // Arrange
-            KeyDerivationPrf prf = KeyDerivationPrf.HMACSHA256;
-            int iterationCount = 10_000;
-            int saltLength = 128;
-            byte[] salt = GetRandomBytes(saltLength);
-            byte[] subkey = GetRandomBytes(128);
+            byte[] salt = this.GetRandomBytes(saltLength);
+            byte[] hash = this.GetRandomBytes(saltLength * 2);
 
             // Act
-            var sut = new PasswordHash(prf, iterationCount, saltLength, salt, subkey);
-            var sut2 = new PasswordHash(sut.Bytes);
-
-            Assert.That(iterationCount, Is.EqualTo(sut.IterationCount).And.EqualTo(sut2.IterationCount));
+            var sut_params_ctor = new PasswordHash(keyDerivationPrf, iterationCount, saltLength, salt, hash);
+            var sut_bytes_ctor = new PasswordHash(sut_params_ctor.Bytes);
 
             // Assert
-            Assert.AreEqual(prf, sut.Prf);
-            Assert.AreEqual(iterationCount, sut.IterationCount);
-            Assert.AreEqual(saltLength, sut.SaltLength);
-            Assert.AreEqual(salt, sut.Salt);
-            Assert.AreEqual(subkey, sut.Subkey);
+            Assert.That(sut_params_ctor, Is.EqualTo(sut_bytes_ctor));
         }
 
-        public void Ctor_Should_TrowArgumentNullException_When_PassedNullArguments()
+        [TestCase(null)]
+        [TestCase(new byte[0])]
+        public void CtorWhichTakesBytesArray_Should_ThrowException_When_BytesArrayIsNullOrEmpty(byte[] bytesArray)
         {
-            Assert.Fail();
+            Assert.That(() => new PasswordHash(bytesArray), Throws.ArgumentNullException);
         }
 
         [Test]
-        public void Ctor_Should_ThrowException_When_SaltLengthDoesNotMatchSalt()
+        public void CtorWhichTakesByteArray_Should_ThrowException_When_SaltLengthDoesMatchLengthOfSaltArray()
         {
-            Assert.Fail();
+            //Assert.That(() => new PasswordHash(bytesArray), Throws.ArgumentNullException);
         }
-
 
         [Test]
-        public void Equals_Should_ReturnTrue_When_HashesAreSame()
+        public void Foo()
         {
-            // Arrange
-            KeyDerivationPrf prf = KeyDerivationPrf.HMACSHA256;
-            int iterationCount = 10_000;
-            int saltLength = 128;
-            byte[] salt = GetRandomBytes(saltLength);
-            byte[] subkey = GetRandomBytes(128);
+            var passwordHasher = new PasswordHasher();
+            var passwordHash = passwordHasher.HashPassword("Admin_123");
 
-            // Act
-            var sut1 = new PasswordHash(prf, iterationCount, saltLength, salt, subkey);
-            var sut2 = new PasswordHash(prf, iterationCount, saltLength, salt, subkey);
-
-            // Assert
-            Assert.IsTrue(sut1.Equals(sut2));
-            Assert.IsTrue(sut1 == sut2);
-            Assert.IsFalse(sut1 != sut2);
+            string base64 = Convert.ToBase64String(passwordHash);
         }
 
+        //[Test]
+        //public void EqualityOperator_ShouldReturnTrue_WhenHashesAreSame()
+        //{
+        //    bool result = true;
 
-        [Test]
-        public void Equals_Should_ReturnFalse_When_HashesAreDifferent()
-        {
-            // Arrange
-            byte[] salt = GetRandomBytes(128);
-            byte[] subkey = GetRandomBytes(128);
+        //    Assert.IsTrue(result);
+        //}
 
-            // Act
-            var sut1 = new PasswordHash(KeyDerivationPrf.HMACSHA256, 10_000, 128, salt, subkey);
-            var sut2 = new PasswordHash(KeyDerivationPrf.HMACSHA256, 1_000, 128, salt, subkey);
+        //[Test]
+        //public void EqualityOperator_ShouldReturnFalse_WhenHashesAreNotSame()
+        //{
+        //    bool result = false;
 
-            // Assert
-            Assert.IsFalse(sut1 == sut2);
-            Assert.IsTrue(sut1 != sut2);
-        }
+        //    Assert.IsFalse(result);
+        //}
+
+        //public void Ctor_Should_TrowArgumentNullException_When_PassedNullArguments()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[Test]
+        //public void Ctor_Should_ThrowException_When_SaltLengthDoesNotMatchSalt()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[Test]
+        //public void Equals_Should_ReturnTrue_When_HashesAreSame()
+        //{
+        //    // Arrange
+        //    KeyDerivationPrf prf = KeyDerivationPrf.HMACSHA256;
+        //    int iterationCount = 10_000;
+        //    int saltLength = 128;
+        //    byte[] salt = GetRandomBytes(saltLength);
+        //    byte[] subkey = GetRandomBytes(128);
+
+        //    // Act
+        //    var sut1 = new PasswordHash(prf, iterationCount, saltLength, salt, subkey);
+        //    var sut2 = new PasswordHash(prf, iterationCount, saltLength, salt, subkey);
+
+        //    // Assert
+        //    Assert.IsTrue(sut1.Equals(sut2));
+        //    Assert.IsTrue(sut1 == sut2);
+        //    Assert.IsFalse(sut1 != sut2);
+        //}
+
+        //[Test]
+        //public void Equals_Should_ReturnFalse_When_HashesAreDifferent()
+        //{
+        //    // Arrange
+        //    byte[] salt = GetRandomBytes(128);
+        //    byte[] subkey = GetRandomBytes(128);
+
+        //    // Act
+        //    var sut1 = new PasswordHash(KeyDerivationPrf.HMACSHA256, 10_000, 128, salt, subkey);
+        //    var sut2 = new PasswordHash(KeyDerivationPrf.HMACSHA256, 1_000, 128, salt, subkey);
+
+        //    // Assert
+        //    Assert.IsFalse(sut1 == sut2);
+        //    Assert.IsTrue(sut1 != sut2);
+        //}
 
         private byte[] GetRandomBytes(int bufferSize)
         {
